79aecbf268ca416767cfbc8fc68a32bb2e616602
Julian Estefan <julian.y.estefan@gmail.comm>
2019-11-15 19:54:07 -0300

    Added action to add a todo in the server

--- web/front-end/js/main.js
+++ web/front-end/js/main.js
@@ -1,17 +1,40 @@
 import { requestApi } from './services/api/todo';
 
+// Ask the server for saved todos
 async function showTodos() {
-  const res =  await requestApi({action: "GetTodos" });
-  const data = await res.json();
+  try{
+    const res = await requestApi({ action: "GetTodos" });
+    const data = await res.json();
+  
+    const list = document.getElementById("myUL");
+  
+    data.forEach(element => {
+      const item = document.createElement("li");
+      item.innerHTML = element
+      list.appendChild(item);
+    });
+  }catch(error){
+    console.log(error);
+  }
+  
+};
 
-  const list = document.getElementById("myUL");
 
-  data.forEach(element => {
-    const item = document.createElement("li" );
-    item.innerHTML = element
-    list.appendChild(item);
-  });
-}
+//Add a todo in the server
+async function showTodos() {
+  const message = document.getElementById("feedback");
+
+  try {
+    const res = await requestApi({ action: "AddTodo" });
+    const data = await res.json();
+    message.innerHTML = "Added todo";
+    console.log("Added todo");
+  }catch(error) {
+    console.log(error);
+    message.innerHTML = "The todo was not added. Try it again later";
+  }
+
+};
 
 
 // Create a "close" button and append it to each list item
@@ -29,7 +52,7 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -37,39 +60,35 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
 }, false);
 
 // Create a new list item when clicking on the "Add" button
-function newElement(event) {
-  debugger;
-  if (event.keyCode === 13  || event.type === "click") {
-    var li = document.createElement("li");
-    var inputValue = document.getElementById("myInput").value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
-    if (inputValue === '') {
-      alert("Tenes que ingresar datos!");
-    } else {
-      document.getElementById("myUL").appendChild(li);
-    }
-    document.getElementById("myInput").value = "";
-  
-    var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
-  
-    for (i = 0; i < close.length; i++) {
-      close[i].onclick = function() {
-        var div = this.parentElement;
-        div.style.display = "none";
-      }
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Tenes que ingresar datos!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
     }
   }
- 
 }
